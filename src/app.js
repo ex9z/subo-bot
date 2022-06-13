261e17684a0cf3c455797d5e228f47cb535dd6c1
ex9z <dansteel53@gmail.com>
2022-06-14 00:53:09 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import tmi from 'tmi.js'
-import { bot_username, oauth_token, channel_name, ban_words } from './constants';
+import { bot_username, oauth_token, channel_name, ban_words} from './constants';
 
 
 const options = {
@@ -17,7 +17,7 @@ const options = {
 	},
 
 	channels: [ channel_name ]
-}
+};
 
 const client = new tmi.Client(options);
 
@@ -28,31 +28,30 @@ client.on('message', (channel, userstate, message, self) => {
 	if(self) return;
 
     if (userstate.username === bot_username) return;
-    
-	if(message.toLowerCase() === 'q') {
-		client.say(channel, `@${userstate.username}, Kissahomie`);
-	}
 
-	if(message.toLowerCase() === '!song') {
-		client.say(channel, `кющ-баран-кющ гип-мири-мющ дющан мющ кющ-баран-кющ кищ мари-нари эээна охуенный военный унунунум кищ-баран-кищ кющ-бараш куш-бараш вот такая вкусная еда от пророка санбоя кищ-баран-кющ кющ-баран-кющ дашан миклющ баращ кищ-баран-кищ кющ-брамбищ-мунуманащ нулащ кущ-барандущ`);
-	}
 
-    checkChat(userstate, message, channel)
 
+	checkChat(userstate, message, channel)
 });
 
 
-function checkChat(usertate, message, channel) {
+function checkChat(userstate, message, channel) {
 
     message = message.toLowerCase()
 
-    let ban_word_sent = false
+    let bannable = false
 
-    ban_word_sent = ban_words.some(bannedwords => message.includes(bannedwords.toLowerCase()))
+    bannable = ban_words.some(bannedwords => message.includes(bannedwords.toLowerCase()))
 
-    if (ban_word_sent){
-        client.say(channel, `@${usertate.username}, docCBT`)
-        client.deletemessage(channel, usertate.id)
+    if (bannable){
+        client.say(channel, `@${userstate.username} docCBT`)
     }
-   
-}
+
+	if(message === '!song') {
+		client.say(channel, `кющ-баран-кющ гип-мири-мющ дющан мющ кющ-баран-кющ кищ мари-нари эээна охуенный военный унунунум кищ-баран-кищ кющ-бараш куш-бараш вот такая вкусная еда от пророка санбоя кищ-баран-кющ кющ-баран-кющ дашан миклющ баращ кищ-баран-кищ кющ-брамбищ-мунуманащ нулащ кущ-барандущ`);
+	}
+
+	if(message === 'q') {
+		client.say(channel, `@${userstate.username} Kissahomie`);
+	}
+};
