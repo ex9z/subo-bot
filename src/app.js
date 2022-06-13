bcd3dab657f0f8f5de440153389091080c86c875
ex9z <dansteel53@gmail.com>
2022-06-13 23:26:01 +0300

    123

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import tmi from 'tmi.js'
+import { bot_username, oauth_token, channel_name, ban_words } from './constants';
+
+
+const options = {
+    options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: bot_username,
+		password: oauth_token
+	},
+	channels: [ channel_name ]
+}
+
+const client = new tmi.Client(options);
+
+client.connect().catch(console.error);
+
+client.on('message', (channel, userstate, message, self) => {
+
+	if(self) return;
+
+    if (userstate.username === bot_username) return;
+    
+	if(message.toLowerCase() === 'q') {
+		client.say(channel, `@${userstate.username}, Kissahomie`);
+	}
+
+    checkChat(userstate, message, channel)
+
+});
+
+
+function checkChat(usertate, message, channel) {
+
+    message = message.toLowerCase()
+
+    let ban_word_sent = false
+
+    ban_word_sent = ban_words.some(bannedwords => message.includes(bannedwords.toLowerCase()))
+
+    if (ban_word_sent){
+        client.say(channel, `@${usertate.username}, durak`)
+        client.deletemessage(channel, usertate.id)
+    }
+   
+}
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+import { bot_name, token, channel, ban } from '/home/subo/subo_bot_constants';
+
+export const channel_name = channel
+export const oauth_token = token
+export const bot_username = bot_name
+export const ban_words = ban
